3f692c9cd1aa2fddffb962ecf0863ee995413096
Ryan Chen <ryan101.chen@gmail.com>
2022-04-24 23:18:55 +0800

    Use cucumber & serenity to create sample
    Signed-off-by: Ryan Chen <ryan101.chen@gmail.com>

---
+++ banking/src/main/java/com/ryan/banking/AccountType.java
@@ -0,0 +1,8 @@
+package com.ryan.banking;
+
+public enum AccountType {
+  Current,
+  Savings,
+  Investment,
+  SuperSaver
+}
---
+++ banking/src/test/java/com/ryan/banking/TransferApi.java
@@ -0,0 +1,24 @@
+package com.ryan.banking;
+
+public class TransferApi {
+
+  double amountToTransfer;
+  BankAccount sourceAccount;
+
+  public TransferApi theAmount(double amountToTransfer) {
+    this.amountToTransfer = amountToTransfer;
+    return this;
+  }
+
+  public TransferApi from(BankAccount sourceAccount) {
+    this.sourceAccount = sourceAccount;
+    return this;
+  }
+
+  public void to(BankAccount destinationAccount) {
+    if (sourceAccount.getBalance() >= amountToTransfer) {
+      sourceAccount.withdraw(amountToTransfer);
+      destinationAccount.deposit(amountToTransfer);
+    }
+  }
+}
